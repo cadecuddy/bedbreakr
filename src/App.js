65af94965d086ca537d4c1a4865b980db3a1a2af
Cade Cuddy <cjcuddy@ncsu.edu>
2021-07-26 16:44:29 -0400

    Resolve all CORS errors with UUID API

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import { useState } from 'react';
 import StatsPanel from './StatsPanel';
 
 export default function App() {
+  const [player, setPlayer] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const api = {
     base: 'https://api.hypixel.net/player?key=',
     key: process.env.REACT_APP_HYPIXEL_API_KEY
   }
-
-  const [player, setPlayer] = useState({});
-  const [loading, setLoading] = useState(true);
 
   const onSearch = async(text) => {
     if (text === '') {
@@ -20,16 +19,17 @@ export default function App() {
     }
     text = text.replace(/\s+/g, '');
 
-    fetch(`https://minecraft-api.com/api/uuid/${text}`, {mode: 'no-cors'})
-      .then(res => res.text())
+    fetch(`https://api.minetools.eu/uuid/${text}`)
+      .then(res => res.json())
       .then(result => {
-        if (result === "Player not found !" || result === "") {
+        if (result.status === "ERR" || result === "") {
           setLoading(true)
           setPlayer(null)
           setLoading(false)
+          console.log(result);
         }
         else {
-            fetch(`${api.base}${api.key}&uuid=${result}`, {mode: 'no-cors'})
+            fetch(`${api.base}${api.key}&uuid=${result.id}`)
             .then(bedwars_res => bedwars_res.json())
             .then(bedwars_json => {
               setLoading(true)
